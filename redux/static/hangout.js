0c331ce7f709ec553bf2b75d48b6951be5429b44
Kyle McDonald <kyle@kylemcdonald.net>
2013-08-26 20:35:09 -0400

    separate logs

--- redux/static/hangout.js
+++ redux/static/hangout.js
@@ -119,7 +119,8 @@ function updateAvatars() {
   participants = gapi.hangout.getParticipants();
   for(i in participants) {
     person = participants[i].person;
-    console.log("updating avatar " + i + " from: " + person);
+    console.log("updating avatar " + i);
+    console.log(person);
     if(person != localPerson) {
       remotePerson = person;
     }
