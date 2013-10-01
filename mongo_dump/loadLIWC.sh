4dae95aac085748609010f56223affed96ed51d9
lauren mccarthy <laurmccarthy@gmail.com>
2013-10-01 16:55:32 -0400

    removing old files, adding icons

--- mongo_dump/loadLIWC.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-mongorestore --db fixus -c LIWC LIWC.bson
-mongorestore --db fixus -c LIWC_wildcards LIWC_wildcards.bson
-echo "loaded LIWC libs into mongodb"
--- mongo_dump/loadLIWC_mongolab.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-mongorestore -h ds039057-a0-internal.mongolab.com:39057 -u sosolimited -p r3c0n12 --db recon -c LIWC LIWC.bson
-mongorestore -h ds039057-a0-internal.mongolab.com:39057 -u sosolimited -p r3c0n12 --db recon -c LIWC_wildcards LIWC_wildcards.bson
-echo "loaded LIWC libs into mongodb_mongolab"
